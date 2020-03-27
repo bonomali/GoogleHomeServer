fac02fb564aaf6eefa3b5b3de41d7c6378b8a17e
jerry lee <jerrylee@jerrys-MacBook-Pro.local>
2020-03-26 19:23:07 -0500

    remove express, added script

---
+++ index.js
@@ -0,0 +1,9 @@
+let http = require('http');
+//create a server object:
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'}); // http header
+    res.write('<h1>Hello World!<h1>'); //write a response
+    res.end(); //end the response
+}).listen(3001, function(){
+    console.log("server start at port 3000"); //the server object listens on port 3000
+});
